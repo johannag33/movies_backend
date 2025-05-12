6ff544b005558f09db3257e5b94d9b43b08b045f
JohannaG33 <johanna.guilhemjouan@gmail.com>
2025-05-12 21:47:04 +0200

    SQLAlchemy model is done

---
+++ api/test_models.py
@@ -0,0 +1,58 @@
+﻿from database import SessionLocal
+from models import Movie, Rating, Tag, Link
+
+db = SessionLocal()
+
+# Tester la récupération de quelques films
+
+movies = db.query(Movie).limit(10).all()
+
+for movie in movies:
+    print(f"ID : {movie.movieId}, Titre : {movie.title}, Genres : {movie.genres}")
+
+
+# Récupérer des films du genre Action
+action_movies = db.query(Movie).filter(Movie.genres.like("%Action%")).limit(5).all()
+
+for movie in action_movies:
+    print(f"ID : {movie.movieId}, Titre : {movie.title}, Genre : {movie.genres}")
+
+
+# tester la récupération de quelques évaluations (ratings)
+
+Ratings = db.query(Rating).limit(20).all()
+
+for rating in Ratings:
+    print(
+        f"UserID : {rating.userId}, MovieID : {rating.movieId}, Rating : {rating.rating}"
+    )
+
+# Récupérer films dont note > 4 :
+high_rated_movies = (
+    db.query(Movie.title, Rating.rating)
+    .join(Rating)
+    .filter(Rating.rating >= 4)
+    .limit(5)
+    .all()
+)
+
+for title, rating in high_rated_movies:
+    print(title, rating)
+
+# Récupérer les tags associés aux films
+
+tags = db.query(Tag.userId, Tag.tag, Movie.title).join(Movie).limit(5).all()
+
+for userId, tag, title in tags:
+    print(f"UserID : {userId}, Tag : {tag}, Title : {title}")
+
+
+# Tester la classe link
+
+links = db.query(Link).limit(5).all()
+
+for link in links:
+    print(f"Movie ID : {link.movieId}, Link : {link.imdbId}, IMG : {link.tmdbId}")
+
+# Fermer la session
+db.close()
---
+++ api/test_query_helpers.py
@@ -0,0 +1,49 @@
+﻿from database import SessionLocal
+from query_helpers import *
+
+db = SessionLocal()
+
+movie = get_movie(db, movie_id=1)
+print(movie.title, movie.genres)
+
+movies = get_movies(db, limit=5)
+for film in movies:
+    print(f"ID : {film.movieId}, Titre : {film.title}, genres : {film.genres}")
+
+
+rating = get_rating(db, user_id=1, movie_id=1)
+print(
+    f"Movie ID : {rating.movieId}, userID : {rating.userId}, rating : {rating.rating} "
+)
+
+ratings = get_ratings(db, min_rating=3.5, limit=10, user_id=3)
+for film in ratings:
+    print(f"ID : {film.movieId}, rating : {film.rating}")
+
+
+tag = get_tag(db, user_id=2, movie_id=60756, tag_text="funny")
+print(tag)
+print(f"User ID : {tag.userId}, Movie ID : {tag.movieId}, Tag : {tag.tag}")
+
+tags = get_tags(db, skip=0, limit=5)
+for film in tags:
+    print(
+        f"GET_TAGS : Movie ID : {film.movieId}, userID : {film.userId}, tag : {film.tag}"
+    )
+
+link = get_link(db, movie_id=1)
+print(link)
+print(f"MovieID = {link.movieId}, link : {link.imdbId}")
+
+links = get_links(db, skip=1, limit=100)
+for film in links:
+    print(f"LINKS - Movie ID : {film.movieId}, Lien : {film.imdbId} - {film.tmdbId}")
+
+n_movies = get_movie_count(db)
+print(n_movies)
+
+n_links = get_link_count(db)
+print(f"Nombre de liens : {n_links}")
+
+# Fermer la session
+db.close()
